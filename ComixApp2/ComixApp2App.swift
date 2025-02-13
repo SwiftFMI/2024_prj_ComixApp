bcf16a4ad6b0cc157d12800ec997f3f35e2847d0
Martina&Galya <martitopp14@gmail.com>
2025-02-13 20:33:37 +0200

    Initial Commit

---
+++ ComixApp2/ComixApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  ComixApp2App.swift
+//  ComixApp2
+//
+//  Created by Alexander Todorov on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ComixApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComixApp2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComixApp2
+//
+//  Created by Alexander Todorov on 2/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world2!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
